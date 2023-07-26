2c74f994efaba3b479d2da72de6817728f48173e
Alexandre Denord <adenord@adenord.local>
2023-07-26 09:37:20 +0200

    lstnew correction

--- srcs/bonus/ft_lstnew.c
+++ srcs/bonus/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: adenord <alexandre.denord@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 17:19:42 by adenord           #+#    #+#             */
-/*   Updated: 2023/07/24 17:48:08 by adenord          ###   ########.fr       */
+/*   Updated: 2023/07/25 22:14:26 by adenord          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*lst;
 
-	lst = (t_list *)ft_calloc(1, sizeof(t_list *));
+	lst = (t_list *)ft_calloc(1, sizeof(t_list));
 	if (!lst)
 		return (NULL);
 	lst[0].next = NULL;
